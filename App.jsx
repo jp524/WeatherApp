dbcd8cb96e3d09cf918c61662dd1f433ce00eec3
Jade <85654561+jp524@users.noreply.github.com>
2024-03-12 13:52:51 -0400

    Add basic layout and styles

--- App.jsx
+++ App.jsx
@@ -1,14 +1,61 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Current Weather</Text>
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.temp}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8</Text>
+          <Text style={styles.highLow}>Low: 6</Text>
+        </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>It's sunny</Text>
+        <Text style={styles.message}>It's perfect t-shirt weather</Text>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: 'pink',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  temp: {
+    color: 'black',
+    fontSize: 48,
+  },
+  feels: {
+    color: 'black',
+    fontSize: 30,
+  },
+  highLow: {
+    color: 'black',
+    fontSize: 20,
+  },
+  highLowWrapper: {
+    flexDirection: 'row',
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+  },
+  description: {
+    color: 'black',
+    fontSize: 48,
+  },
+  message: {
+    color: 'black',
+    fontSize: 30,
+  },
+});
 
 export default App;
