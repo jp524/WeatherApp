b14f613ab9c64db7adde792769008a9e77fad970
Jade <85654561+jp524@users.noreply.github.com>
2024-03-13 09:47:39 -0400

    Add @react-navigation/bottom-tabs package

--- App.jsx
+++ App.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import CurrentWeather from './src/screens/CurrentWeather';
 import UpcomingWeather from './src/screens/UpcomingWeather';
 import City from './src/screens/City';
-import Icon from 'react-native-vector-icons/Feather';
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <CurrentWeather />
-      </View>
+      <Tab.Navigator>
+        <Tab.Screen name={'Current'} component={CurrentWeather} />
+        <Tab.Screen name={'Upcoming'} component={UpcomingWeather} />
+        <Tab.Screen name={'City'} component={City} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
