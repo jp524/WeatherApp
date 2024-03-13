a80fe454fecef70cc4de2c532a3bbbc9e7f28889
Jade <85654561+jp524@users.noreply.github.com>
2024-03-13 09:36:35 -0400

    Add @react-navigation/native package

--- App.jsx
+++ App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 import CurrentWeather from './src/screens/CurrentWeather';
 import UpcomingWeather from './src/screens/UpcomingWeather';
 import City from './src/screens/City';
 import Icon from 'react-native-vector-icons/Feather';
+
 const App = () => {
   return (
-    <View style={styles.container}>
-      <CurrentWeather />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <CurrentWeather />
+      </View>
+    </NavigationContainer>
   );
 };
 
