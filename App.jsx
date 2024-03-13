f6a6c1560fff5ebc3872f95d15298e88aa04a62a
Jade <85654561+jp524@users.noreply.github.com>
2024-03-13 11:09:43 -0400

    Add loading state to App component

--- App.jsx
+++ App.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {ActivityIndicator, View, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import Tabs from './src/components/Tabs';
 
 const App = () => {
-  return (
+  const [loading, setLoading] = useState(true);
+
+  const loadingView = (
+    <View style={styles.container}>
+      <ActivityIndicator size={'large'} color={'blue'} />
+    </View>
+  );
+
+  const notLoadingView = (
     <NavigationContainer>
       <Tabs />
     </NavigationContainer>
   );
+
+  return loading ? loadingView : notLoadingView;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+  },
+});
 
 export default App;
