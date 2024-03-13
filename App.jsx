72ed9fcc5fc23f082a293bac71a7dd1a9d00ea3d
Jade <85654561+jp524@users.noreply.github.com>
2024-03-13 13:59:11 -0400

    Refactor code to extract useGetWeather hook

--- App.jsx
+++ App.jsx
@@ -1,57 +1,27 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {ActivityIndicator, View, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import Geolocation from '@react-native-community/geolocation';
 import Tabs from './src/components/Tabs';
-import {WEATHER_API_KEY} from '@env';
+import {useGetWeather} from './src/hooks/useGetWeather';
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [weather, setWeather] = useState([]);
-  const [lat, setLat] = useState([]);
-  const [lon, setLon] = useState([]);
+  const [loading, errorMessage, weather] = useGetWeather();
 
-  useEffect(() => {
-    Geolocation.getCurrentPosition(
-      position => {
-        setLat(position.coords.latitude);
-        setLon(position.coords.longitude);
-      },
-      error => {
-        console.log(error);
-      },
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'blue'} />
+      </View>
     );
-    fetchWeatherData();
-  }, [lat, lon]);
+  }
 
-  const fetchWeatherData = async () => {
-    try {
-      const res = await fetch(
-        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`,
-      );
-      const data = await res.json();
-      setWeather(data);
-    } catch (error) {
-      setErrorMessage('Could not fetch weather');
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const loadingView = (
-    <View style={styles.container}>
-      <ActivityIndicator size={'large'} color={'blue'} />
-    </View>
-  );
-
-  const notLoadingView = (
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
-  );
-
-  return loading ? loadingView : notLoadingView;
+  if (weather && weather.list) {
+    return (
+      <NavigationContainer>
+        <Tabs />
+      </NavigationContainer>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
---
+++ src/hooks/useGetWeather.js
@@ -0,0 +1,42 @@
+import {useState, useEffect} from 'react';
+import Geolocation from '@react-native-community/geolocation';
+import {WEATHER_API_KEY} from '@env';
+
+export const useGetWeather = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [weather, setWeather] = useState([]);
+  const [lat, setLat] = useState([]);
+  const [lon, setLon] = useState([]);
+
+  const fetchWeatherData = async () => {
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`,
+      );
+      const data = await res.json();
+      setWeather(data);
+    } catch (err) {
+      setError('could not fetch weather');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      Geolocation.getCurrentPosition(
+        position => {
+          setLat(position.coords.latitude);
+          setLon(position.coords.longitude);
+        },
+        error => {
+          console.log(error);
+        },
+      );
+      await fetchWeatherData();
+    })();
+  }, [lat, lon]);
+
+  return [loading, error, weather];
+};
