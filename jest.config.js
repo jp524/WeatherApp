542eadc29cc284a7b8ce49720a0b8c2fbfc7ac3d
Jade <85654561+jp524@users.noreply.github.com>
2024-03-14 13:10:03 -0400

    Set up Jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: 'react-native',
+  transform: {'^.+\\.(js|jsx)$': 'babel-jest'},
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|@react-navigation|react-native-vector-icons)/)',
+  ],
 };
