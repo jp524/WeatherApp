990232b9b96be8695db4739ee61a88057d81f136
Jade <85654561+jp524@users.noreply.github.com>
2024-03-14 13:34:15 -0400

    Add @testing-library packages

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transform: {'^.+\\.(js|jsx)$': 'babel-jest'},
   transformIgnorePatterns: [
     'node_modules/(?!(@react-native|react-native|@react-navigation|react-native-vector-icons)/)',
